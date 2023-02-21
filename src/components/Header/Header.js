b722095d66e28c9e74e41555621d3256b1298d40
NiteChill <puissantachille@gmail.com>
2023-02-21 01:27:40 +0100

    some changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
       <h1 className='c-black500 fs-25 ff-roboto fw-400'>Donit</h1>
       <div className='d-flex-row ai-center'>
         <p className='c-black500 fs-14 ff-roboto fw-400 mr-15'>Antoine Fayeux</p>
-        <img src={pofilePicture} alt='profile' className='w-30 h-30 bora-full border-2 border-white700' />
+        <img src={pofilePicture} alt='profile' className='w-30 h-30 bora-full border-2 border-white700 user-select-none' />
       </div>
     </header>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,21 +11,21 @@ export default function Navbar() {
   return (
     <nav className='d-flex-column w-280 b-white400 border-right-2 border-white500 h-full'>
       <div className='d-flex-row ai-center pl-20 h-50 border-bottom-2 border-white500 fs-14 fw-400 ff-roboto'>
-        <p className='pr-15 border-right-2 border-white500 c-black500'>
+        <p className='pr-15 border-right-2 border-white500 c-black500 user-select-none'>
           My todo
         </p>
         <div className='d-flex-row ai-center pt-10 pr-15 pb-10 pl-15'>
           <img
             src={glass}
             alt='glass'
-            className='w-16 h-16 mr-10 cursor-pointer'
+            className='w-16 h-16 mr-10 cursor-pointer user-select-none'
             onClick={() => search.current.focus()}
           />
           <input
             type='text'
             name='search'
             placeholder='Search'
-            className='c-black500 input-clear w-120'
+            className='c-black500 input-clear w-120 user-select-none'
             ref={search}
             onInput={(e) => setSearchValue(e.target.value)}
             onFocus={(e) => e.target.select()}
